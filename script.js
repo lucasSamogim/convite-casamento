543a1de650dc74a63478402fcf4e3a92824b58c5
lsamogim <lsamogim.dev@gmail.com>
2025-04-05 00:38:46 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,40 @@
+lucide.createIcons();
+
+const audio = document.getElementById("musicaFundo");
+const toggleBtn = document.getElementById("audioToggle");
+
+// Define volume inicial
+audio.volume = 0.2;
+
+toggleBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    toggleBtn.textContent = "⏸️"; // muda para pause
+  } else {
+    audio.pause();
+    toggleBtn.textContent = "▶️"; // muda para play
+  }
+  });
+
+  //Countdown para o Casamento
+
+  function updateCountdown() {
+    const weddingDate = new Date("2025-06-21T16:00:00");
+    const now = new Date();
+    const diff = weddingDate - now;
+
+    if (diff <= 0) return;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    document.getElementById("days").textContent = String(days).padStart(2, "0");
+    document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+    document.getElementById("minutes").textContent = String(minutes).padStart(2, "0");
+    document.getElementById("seconds").textContent = String(seconds).padStart(2, "0");
+  }
+
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
